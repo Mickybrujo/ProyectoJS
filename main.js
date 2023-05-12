45e0a61d3f5ac6e1cd553ddd0e7d0ce1d921b9e1
mickybrujo <micky_brujo_maiden@live.con.mx>
2023-05-12 15:14:20 -0600

    Interaccion de menu deplegable para version desktop y mobile

--- main.js
+++ main.js
@@ -3,13 +3,20 @@
 //Recoleccion de datos del elementos del HTMl
 const menuEmail = document.querySelector('.navbar-email') ;
 const desktopMenu = document.querySelector('.desktop-menu') ;
+const burgerMenu = document.querySelector('.menu') ;
+const mobileMenu = document.querySelector('.mobile-menu') ;
 
 //Interaccion para mostrar menu desplegable al hacer click
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
 
 //Funcion toggle para menu desplegable
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
 
 
