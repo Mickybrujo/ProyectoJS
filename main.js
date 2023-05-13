4cdb5496d21d3a8e3efc208bdedee910e445e0d2
mickybrujo <micky_brujo_maiden@live.con.mx>
2023-05-13 02:19:00 -0600

    Funcionalidad para mostrar los articulos de manera dinamica con JS.

--- main.js
+++ main.js
@@ -1,34 +1,106 @@
 //Proyecto de integracion de Js en una pagina web de compras online
 
 //Recoleccion de datos del elementos del HTMl
-const menuEmail = document.querySelector('.navbar-email') ;
-const desktopMenu = document.querySelector('.desktop-menu') ;
-const burgerMenu = document.querySelector('.menu') ;
-const mobileMenu = document.querySelector('.mobile-menu') ;
-const menuCarritoIcon = document.querySelector('.navbar-shopping-cart') ;
-const asideCarrito = document.querySelector('.product-detail') ;
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const burgerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const asideCarrito = document.querySelector(".product-detail");
+const cardsContainer = document.querySelector(".cards-container");
 
 //Interaccion para mostrar menu desplegable al hacer click
-menuEmail.addEventListener('click', toggleDesktopMenu);
-burgerMenu.addEventListener('click', toggleMobileMenu);
-menuCarritoIcon.addEventListener('click', toggleAsideCarrito);
+menuEmail.addEventListener("click", toggleDesktopMenu);
+burgerMenu.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleAsideCarrito);
 
-//Funcion toggle para menu desplegable
+//Funciones para menus desplegables
 function toggleDesktopMenu() {
-    asideCarrito.classList.add('inactive');
-    desktopMenu.classList.toggle('inactive');
+  asideCarrito.classList.add("inactive");
+  desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-    asideCarrito.classList.add('inactive');
-    mobileMenu.classList.toggle('inactive');
+  asideCarrito.classList.add("inactive");
+  mobileMenu.classList.toggle("inactive");
 }
 
-function toggleAsideCarrito() { 
-    desktopMenu.classList.add('inactive');//Ocultar el menu de usuario
-    mobileMenu.classList.add('inactive'); //Ocultar el menu desplegable
-    asideCarrito.classList.toggle('inactive'); //Mostar y ocultar el aside
+function toggleAsideCarrito() {
+  desktopMenu.classList.add("inactive"); //Ocultar el menu de usuario
+  mobileMenu.classList.add("inactive"); //Ocultar el menu desplegable
+  asideCarrito.classList.toggle("inactive"); //Mostar y ocultar el aside
 }
 
+//Mostrar los productos
 
+//Creacion de un array para almacenar los productos
+const productList = [];
 
+//Simulacion de una base de datos para agregar los productos
+
+for (let i = 0; i < 50; i++) {
+  productList.push({
+    name: "Bike",
+    price: 1001 + i,
+    img: "https://cdn.shopify.com/s/files/1/0541/0154/1047/products/0711964_b_1200x1200.jpg?v=1614971567",
+  });
+  productList.push({
+    name: "Moto",
+    price: 1002 + i,
+    img: "https://fratelliglobal.com/wp-content/uploads/2021/12/Moto-Fratelli-FS-110-Semi-automatica-1.jpg",
+  });
+  productList.push({
+    name: "Auto",
+    price: 1003 + i,
+    img: "https://acroadtrip.blob.core.windows.net/catalogo-imagenes/xl/RT_V_05a99b23df054e0eb2c93b3885eaa13e.jpg",
+  });
+  [];
+}
+
+//Creacion de los elementos HTML para mostrar los productos
+
+//Funcion para renderizar los productos
+function renderProducts(productList) {
+  //Recorrer el array de productos
+  for (product of productList) {
+    //Contenedor de la tarjeta del producto
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    //Imagen del producto
+    const imgProduct = document.createElement("img");
+    imgProduct.setAttribute("src", product.img);
+
+    //Contenedor de la informacion del producto
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    //Contenedor de los datos del producto
+    const productInfoDiv = document.createElement("div");
+
+    //Parrafos para agregar los datos del producto (nombre y precio)
+    const productPrice = document.createElement("p");
+    productPrice.innerText = "$" + product.price;
+
+    const productName = document.createElement("p");
+    productName.innerText = product.name;
+
+    //Creacion de la figura para el icono de agregar al carrito
+    const productInfoFigure = document.createElement("figure");
+    const productFigureImg = document.createElement("img");
+    productFigureImg.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    //Maquetacion de los elementos HTML para organizar los elementos
+    productInfoFigure.appendChild(productFigureImg);
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+    productCard.appendChild(imgProduct);
+    productCard.appendChild(productInfo);
+    cardsContainer.appendChild(productCard);
+  }
+}
+
+//Llamado a la funcion para mostrar los productos pasando el array de productos
+renderProducts(productList);
